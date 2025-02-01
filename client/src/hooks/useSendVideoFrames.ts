b8c68901e3a2e4b6cc156c79d5c616a52c7ce475
cassiocappellari <cassiocappellari@gmail.com>
2025-02-01 11:17:39 +0100

    feat: create custom hook responsible for sending video frames to the api

---
+++ client/src/hooks/useSendVideoFrames.ts
@@ -0,0 +1,43 @@
+import { useRef, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { objectsDetectionRequest } from "../services/pythonApi";
+import { setBoundingBox } from "../store/frameCanvasSlice";
+
+const useSendVideoFrames = () => {
+  const dispatch = useDispatch();
+  const confidence = useSelector((state: RootState) => state.configPanel.confidence);
+  const iou = useSelector((state: RootState) => state.configPanel.iou);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const captureFrameAndSendToAPI = useCallback(async () => {
+    const frameName = 1;
+
+    const requestData = {
+      image_path: `./captured_frames/000${frameName}.jpg`,
+      confidence,
+      iou,
+    };
+
+    const result = await objectsDetectionRequest(requestData);
+    dispatch(setBoundingBox(result));
+  }, [confidence, iou, dispatch]);
+
+  const handleVideoPlay = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(captureFrameAndSendToAPI, 1000);
+  };
+
+  const handleVideoPause = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  return { handleVideoPlay, handleVideoPause };
+};
+
+export default useSendVideoFrames;
