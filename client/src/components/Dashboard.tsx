764c09615e92b5fa37cb01bac83824ca7243e942
cassiocappellari <cassiocappellari@gmail.com>
2025-02-03 08:53:44 +0100

    feat: add a new header on the app dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -5,21 +5,36 @@ import ResultsTable from "./ResultsTable";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-[2fr_1fr] gap-4 p-4 h-screen">
-      <div className="bg-white rounded-lg shadow p-4">
-        <VideoPlayer />
-      </div>
+    <div className="h-screen flex flex-col bg-gray-900 text-white">
+      <header className="flex items-center justify-between bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <img src="/overview-ai-logo.png" alt="Company Logo" className="h-10" />
+          <h1 className="absolute left-1/2 -translate-x-1/2 text-xl font-semibold text-indigo-700">
+            Dashboard
+          </h1>
+        </div>
+        <div className="flex gap-4">
+          <button className="bg-indigo-700 px-4 py-2 rounded-sm">Sign In</button>
+          <button className="bg-red-700 px-4 py-2 rounded-sm">Sign Out</button>
+        </div>
+      </header>
 
-      <div className="bg-white rounded-lg shadow p-4">
-        <FrameCanvas />
-      </div>
+      <div className="grid grid-cols-2 grid-rows-[2fr_1fr] gap-4 p-4 flex-grow">
+        <div className="bg-indigo-950 rounded-lg shadow p-4">
+          <VideoPlayer />
+        </div>
 
-      <div className="bg-white rounded-lg shadow p-4">
-        <ConfigPanel />
-      </div>
+        <div className="bg-indigo-950 rounded-lg shadow p-4">
+          <FrameCanvas />
+        </div>
 
-      <div className="bg-white rounded-lg shadow p-4">
-        <ResultsTable />
+        <div className="bg-indigo-950 rounded-lg shadow p-4">
+          <ConfigPanel />
+        </div>
+
+        <div className="bg-indigo-950 rounded-lg shadow p-4">
+          <ResultsTable />
+        </div>
       </div>
     </div>
   );
