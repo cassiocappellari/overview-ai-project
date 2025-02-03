1b9d62eac8a9b63b55fb8f18435a405a092d5126
cassiocappellari <cassiocappellari@gmail.com>
2025-02-03 17:32:12 +0100

    feat: add a new load spinner on the frames section

--- client/src/components/FrameCanvas.tsx
+++ client/src/components/FrameCanvas.tsx
@@ -18,6 +18,7 @@ const FrameCanvas: React.FC = () => {
     const [selectedFrame, setSelectedFrame] = useState<string | null>(null);
     const [selectedFramePredictions, setSelectedFramePredictions] = useState<BoundingBox[][]>();
     const [frameRefsAndPredictions, setFrameRefsAndPredictions] = useState<Map<string, BoundingBox[][]>>()
+    const isVideoLoading = useSelector((state: RootState) => state.videoPlayer.isVideoLoading);
 
     useEffect(() => {
         if (!selectedFramePredictions) return;
@@ -134,40 +135,46 @@ const FrameCanvas: React.FC = () => {
 
     return (
         <div className="App">
-            <div className="p-4 bg-indigo-900 shadow rounded-lg">
-                <div className="text-center">
-                    <h3 className="text-lg font-bold mb-4">Preview Area</h3>
-                    <span>{extractFileName(selectedFrame)}</span>
-                </div>
-                <div>
-                    <canvas id="canvas" ref={canvasRef} />
-                </div>
+          <div className="p-4 bg-indigo-900 shadow rounded-lg">
+            <div className="text-center">
+              <h3 className="text-lg font-bold mb-4">Preview Area</h3>
+              <span>{extractFileName(selectedFrame)}</span>
             </div>
-
-            <div className="p-4 bg-indigo-900 shadow rounded-lg mt-8">
-                <h3 className="text-m font-bold text-center mb-4">Select a Frame</h3>
-                {predictionResults.length === 0 ? (
-                    <div className="flex items-center justify-center w-full h-24 border-2 border-dashed text-white">
-                    <span>No frames available</span>
-                    </div>
-                ) : (
-                    <div className="w-full overflow-x-auto rounded-lg">
-                        <div className="flex space-x-4 p-2" style={{ minWidth: "max-content" }}>
-                            {frames.map((frameSrc, index) => (
-                            <img
-                                key={index}
-                                src={frameSrc}
-                                alt={`Frame ${index + 1}`}
-                                className="w-[140px] h-[80px] object-cover cursor-pointer border border-gray-300 rounded-md"
-                                onClick={() => handleFrameSelect(frameSrc)}
-                            />
-                            ))}
-                        </div>
-                    </div>
-                )}
+            <div>
+              <canvas id="canvas" ref={canvasRef} />
+            </div>
+          </div>
+    
+          <div className="p-4 bg-indigo-900 shadow rounded-lg mt-8">
+            <h3 className="text-m font-bold text-center mb-4">Select a Frame</h3>
+    
+            { isVideoLoading ? (
+              <div className="flex flex-col items-center justify-center w-full h-24">
+                <div className="spinner-border animate-spin border-4 border-blue-500 border-t-transparent rounded-full w-8 h-8 mb-2" />
+                <p className="text-white">Loading...</p>
+              </div>
+            ) : predictionResults.length === 0 ? (
+              <div className="flex items-center justify-center w-full h-24 border-2 border-dashed text-white">
+                <span>No frames available</span>
+              </div>
+            ) : (
+              <div className="w-full overflow-x-auto rounded-lg">
+                <div className="flex space-x-4 p-2" style={{ minWidth: "max-content" }}>
+                  {frames.map((frameSrc, index) => (
+                    <img
+                      key={index}
+                      src={frameSrc}
+                      alt={`Frame ${index + 1}`}
+                      className="w-[140px] h-[80px] object-cover cursor-pointer border border-gray-300 rounded-md"
+                      onClick={() => handleFrameSelect(frameSrc)}
+                    />
+                  ))}
                 </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      );
 };
 
 export default FrameCanvas;
